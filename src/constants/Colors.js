0044576c0d79f1d93831e392f366170cfa313044
Dev <dev@Devs-Mac-mini.local>
2020-09-16 23:21:59 -0400

    Fix the conflict

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,25 +1,7 @@
 export default {
   black: 'rgba(0, 0, 0, 1)',
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
   gray: 'rgba(128, 128, 128, 0.9)',
   pink: 'rgba(255, 192, 203, 1)',
->>>>>>> Stashed changes
-=======
-  gray: 'rgba(128, 128, 128, 0.9)',
-  pink: 'rgba(255, 192, 203, 1)',
->>>>>>> Stashed changes
-=======
-  gray: 'rgba(128, 128, 128, 0.9)',
-  pink: 'rgba(255, 192, 203, 1)',
->>>>>>> Stashed changes
-=======
-  gray: 'rgba(128, 128, 128, 0.9)',
-  pink: 'rgba(255, 192, 203, 1)',
->>>>>>> Stashed changes
   red: 'rgba(255, 0, 0, 1)',
   white: 'rgba(255, 255, 255, 1)',
 };
